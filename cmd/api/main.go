839c1fd30cf2891314e95887fbf2624f48616118
thiendsu2303 <vithien23@gmail.com>
2025-04-05 14:49:49 +0700

    adding config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,9 +12,9 @@ const version = "0.0.1"
 
 func main() {
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr: env.GetString("ADDR", ":6065"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://adminaudioai:audious@localhost/audioai?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
